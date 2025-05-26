8156d41b3a270ed8a7dcbeab2ba63c196f2153a2
Juanpllt <juanpablotobon53@gmail.com>
2025-05-26 16:25:28 -0500

    Creacion de conexion con Neon database para tener la base de datos hosteada en la nube

--- src/main/java/co/edu/uco/pathorder/PruebaConexion.java
+++ src/main/java/co/edu/uco/pathorder/PruebaConexion.java
@@ -6,12 +6,13 @@ import java.sql.DriverManager;
 public class PruebaConexion {
 
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/pathorder_db";
-        String usuario = "postgres";
-        String clave = "admin";
+        // URL adaptada para conexión segura a Neon (requiere SSL)
+        String url = "jdbc:postgresql://ep-square-butterfly-a8qzh9rh-pooler.eastus2.azure.neon.tech:5432/pathorder_db?sslmode=require";
+        String usuario = "pathorder_db_owner";
+        String clave = "npg_n0LwiyVuk1dq";
 
         try (Connection conexion = DriverManager.getConnection(url, usuario, clave)) {
-            System.out.println("✅ Conexión exitosa a la base de datos");
+            System.out.println("✅ Conexión exitosa a la base de datos Neon PostgreSQL");
         } catch (Exception exception) {
             System.out.println("❌ Error al conectar:");
             exception.printStackTrace();
